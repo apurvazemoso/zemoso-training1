9641e2f533e786e5d425c022c0cb9d3650bf797f
apurvazemoso <apurva.singhal@zemosolabs.com>
2017-07-10 18:15:28 +0530

    Add block comment to explain the error.

--- Data.java
+++ Data.java
@@ -4,6 +4,13 @@ package apurva.assignment;
 		static int a;
 		static char ch;
 
+		/*The Default intialization allows assignment of default values to the uninitialized 
+		class fields(variables) . But if the variables are defined inside a method inside a class, 
+		the default initialization doesn't occur and it throws an error. So, in the Data.java, 
+		the second method of printing local method's variable doesnt work, while the first 
+		method work because it had class fields printed which was initialized automatically.
+		*/
+
 	public static void printFields(){
 		System.out.println(a);
 		System.out.println(ch);
