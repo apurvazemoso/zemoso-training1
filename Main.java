0ead335b44b5cc53d8048e41d9bfb9959956c13e
apurvazemoso <apurva.singhal@zemosolabs.com>
2017-07-10 17:56:17 +0530

    Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+package apurva.assignment;
+import apurva.assignment.*;
+
+class Main{
+	public static void main(String[] args) {
+		Data firstObj = new Data();
+		firstObj.printFields();
+		firstObj.printLocals();
+		Singleton check = new Singleton();
+		check.methodTwo();
+	}
+	
+}
