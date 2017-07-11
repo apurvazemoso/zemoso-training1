026201f354758ad3285cc23781acc32e11bb100f
apurvazemoso <apurva.singhal@zemosolabs.com>
2017-07-11 11:49:31 +0530

    Add comment in Singleton.java

--- Singleton.java
+++ Singleton.java
@@ -3,6 +3,11 @@ package apurva.assignment;
 	public class Singleton {
 		String nonStaticString;
 
+		
+		/*Initialization of a non static string is not possible from a static method directly, 
+		but it is possible if we make object of the class and then initialize the non static field 
+		from any type of method.*/
+
 		public static Singleton methodOne(String string1){
 			
 			Singleton objectOfClass = new Singleton();
