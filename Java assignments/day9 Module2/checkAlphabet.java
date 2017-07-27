66edbe071302440b0fecabe46b6e83d34a46b43b
apurvazemoso <apurva.singhal@zemosolabs.com>
2017-07-27 15:09:16 +0530

    Improved Alphabet check assignment

--- Java assignments/day9 Module2/checkAlphabet.java
+++ Java assignments/day9 Module2/checkAlphabet.java
@@ -1,43 +1,64 @@
-import java.util.*;  
-import java.lang.*;
-/*The time complexity is string length + 56 since HashMap's containsKey only has O(1) complexity.
-Space complexity is O(String Length + 26).
-We store letters of string in hashmap(not repeating keys), then check from each character of the alphabets whether it is in the HashMap or not.
-If it is not present, we break the string and say it is not complete string
+package checkAlpha;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+/*
+The worst case time complexity is string length + 52 since HashMap's containsKey only has O(1) complexity.
+Space complexity is O(26).
+We store 26 total alphabets in hashmap, then check true for each character of the letter of the input string in the hashmap.
+If any entry in hashmap is checked false, we return incomplete string.
 */
-class checkAlphabet {
-    public static void main(String[] args) {
-		String mainAlpha = "abcdefghijklmnopqrstuvwxyz";
-		String test = "abcdefghiJKLM";
-		test = test.replaceAll("[^A-Za-z]","");
-		if(test.length() < 26) {
-				System.out.println("Not complete string.");
-				System.exit(0);
-		}
-		char[] alphabets = test.toCharArray();
-		int checkk = 1;
-		Map<Character, Integer> m = new HashMap<Character, Integer>();
 
-		//this for loop fills the characters of test string in hashmap in lowerCase irrespective of the actual case of the letter.
+class CheckString{
+
+	CheckString(String test, String mainAlpha){
+
+		if(test.length() < 26) {
+			System.out.println("Not complete string.");
+			System.exit(0);
+			}
+
+		int checkk = 1;
+
+		Map<Character, Boolean> m = new HashMap<Character, Boolean>();
+
+		for (int y = 0; y < mainAlpha.length(); y++){
+				m.put(mainAlpha.charAt(y), false);
+			}
+
 		for (int y = 0; y < test.length(); y++){
-			if(m.containsKey(Character.toLowerCase(alphabets[y]))){}
-			else{
-				m.put(Character.toLowerCase(alphabets[y]), 1);
+			char g = Character.toLowerCase(test.charAt(y));
+			if(m.containsKey(g)) {
+				if(m.get(g) == Boolean.FALSE){
+				    m.put(g, true);
+				    }
 			}
 		}
-		
-		char[] alphabet = mainAlpha.toCharArray();
-		//System.out.println(test + " " + test.length());
 
-		//this for loop check alphabets one by one. If any alphabet is not present it will break the loop and print the same.
-		for(int y = 0; y < mainAlpha.length(); y++){
-			char ch = alphabet[y];
-			if(m.containsKey(ch)) checkk = 1; 
-			else{
-				checkk = 0; System.out.println("Not complete string.");
+		for (Map.Entry<Character, Boolean> entry : m.entrySet())
+		{
+			if(entry.getValue() == false) {
+				checkk = 0;
 				break;
 			}
 		}
-		if (checkk == 1) System.out.println("Great! Complete string!");
+
+		if(checkk == 0) System.out.println("Not complete string.");
+
+		else System.out.println("Great! Complete string.");
+		}
+}
+
+
+
+public class checkAlphabet {
+    public static void main(String[] args) {
+		String test;
+		System.out.println("Enter the string you want to test : ");
+		Scanner input = new Scanner(System.in);
+		test = input.next();
+		String mainAlpha = "abcdefghijklmnopqrstuvwxyz";
+		CheckString a = new CheckString(test, mainAlpha);
+		
 	}
 }
